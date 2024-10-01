a8a17535de6d42760d737201a6f7b1101bdb06f4
NFPsy <141744857+NFPsy@users.noreply.github.com>
2024-10-01 15:28:46 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,7 @@
 function solution(numbers) {
-    // 1부터 9까지 더해서 배열의 합을 빼보자
+    // 1부터 9까지 더해서 배열의 합을 빼기
     // reduce 함수 사용
-    return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
+    // 45는 1~9 합
+    // numbers 배열의 합을 구해서 45를 뺀다.
+    return 45 - numbers.reduce((acc, cur) => acc + cur, 0);
 }
