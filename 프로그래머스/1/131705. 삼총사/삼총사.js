f32b3ce194a129eb9983109aa6f0b89747c72a43
NFPsy <141744857+NFPsy@users.noreply.github.com>
2024-10-20 16:26:04 +0900

    [level 1] Title: 삼총사, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,23 +1,16 @@
 function solution(number) {
     let answer = 0;
+    const n = number.length;
 
-    function backtrack(start, count, sum) {
-        if (count === 3 && sum === 0) {
-            answer++;
-            return;
-        }
-
-        if (count > 3 || start >= number.length) {
-            return;
-        }
-
-        for (var i = start; i < number.length; i++) {
-            var current = number[i];
-            backtrack(i + 1, count + 1, sum + current);
+    for (let i = 0; i < n - 2; i++) {
+        for (let j = i + 1; j < n - 1; j++) {
+            for (let k = j + 1; k < n; k++) {
+                if (number[i] + number[j] + number[k] === 0) {
+                    answer++;
+                }
+            }
         }
     }
-
-    backtrack(0, 0, 0);
 
     return answer;
 }
